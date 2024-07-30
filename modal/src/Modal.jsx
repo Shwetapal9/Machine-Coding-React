74d8f68c1b13439338b4ef89d14559a7ba69a921
Shweta Pal <shwetapal975@gmail.com>
2024-07-30 18:19:08 +0000

    changed for backdrop-- modal closing on clicking outside the modal

--- modal/src/Modal.jsx
+++ modal/src/Modal.jsx
@@ -1,7 +1,14 @@
 const Modal =({setIsOpen})=>{
-    return <div className="modal" onClick={()=>setIsOpen(false)}>
+    return( 
+    <>
+    <div className="backdrop" onClick={()=>setIsOpen(false)}>
+        </div>
+        <div  className="modal">
         <div className="close-modal" onClick={()=>setIsOpen(false)}>X</div>
         <h3>Hello Modal</h3>
-    </div>
+        </div>
+        
+    </>
+    )
 }
 export default Modal
